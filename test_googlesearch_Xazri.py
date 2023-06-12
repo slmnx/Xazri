e3a0fb43c4b8baf64c3c76bf0e159dd5ceb91ff7
Xazri <slmnx94@gmail.com>
2023-06-12 20:46:18 +0300

    Поправил фикстуру.

--- test_googlesearch_Xazri.py
+++ test_googlesearch_Xazri.py
@@ -3,19 +3,19 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session", autouse=True)
 def set_window_size():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
 
 def test_positive_search():
-    browser.open('https://google.com')
+    browser.open('https://google.ru')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
 def test_negative_search():
-    browser.open('https://google.com')
+    browser.open('https://google.ru')
     browser.element('[name="q"]').should(be.blank).type('myaumyau').press_enter()
     browser.element('[id="search"]').should(have.text('Page not Found'))
